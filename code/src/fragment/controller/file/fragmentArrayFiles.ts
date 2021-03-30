fe71694d1960d3723d1115816358371c05b9b8e4
Ryan Kelker <1487241+runexec@users.noreply.github.com>
2021-03-30 15:15:45 -0400

    Refactor File

---
+++ code/src/fragment/controller/file/fragmentArrayFiles.ts
@@ -0,0 +1,8 @@
+import * as Group from '../group';
+import * as FileName from '../../view/fragment/fileName';
+
+export const asArray = ({file_name}:{file_name: string}): string[] => {
+		const group = Group.fragmentArrayGroup(file_name),
+			fragments = group ? group.split(', ') : false;
+		return fragments ? fragments.map(file_name => FileName.fragmentFileName(file_name)) : [];
+	};
---
+++ code/src/fragment/controller/file/fragmentListFiles.ts
@@ -0,0 +1,12 @@
+import * as Tag from '../tag';
+import * as FileName from '../../view/fragment/fileName';
+
+export const asArray = ({file_name}:{file_name: string}): string[] => {
+    const group = Tag.fragmentListTag(file_name);
+    let fragments: string[] | undefined;
+    if (group) {
+        const m = group.match(/\[(.*)\]/);
+        m && (fragments = m[1].split(', '));
+    }
+    return fragments ? fragments.map(file_name => FileName.fragmentListFileName(file_name)) : [];
+};
