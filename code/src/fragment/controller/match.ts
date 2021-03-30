0be4acd69886537a2523c2665fb6f11275fc6979
Ryan Kelker <1487241+runexec@users.noreply.github.com>
2021-03-30 16:51:20 -0400

    Refactor controller

--- code/src/fragment/controller/match.ts
+++ code/src/fragment/controller/match.ts
@@ -1,6 +1,6 @@
-export * as MatchFragment from './match/fragmentMatch';
-export * as MatchFragmentArray from './match/fragmentArrayMatch';
-export * as MatchFragmentList from './match/fragmentListMatch';
-export * as MatchFragmentLive from './match/fragmentLiveMatch';
-export * as MatchFragmentLiveArray from './match/fragmentLiveArrayMatch';
-export * as MatchFragmentLiveList from './match/fragmentLiveListMatch';
+export * as FragmentMatch from './match/fragmentMatch';
+export * as FragmentArrayMatch from './match/fragmentArrayMatch';
+export * as FragmentListMatch from './match/fragmentListMatch';
+export * as FragmentLiveMatch from './match/fragmentLiveMatch';
+export * as FragmentLiveArrayMatch from './match/fragmentLiveArrayMatch';
+export * as FragmentLiveListMatch from './match/fragmentLiveListMatch';
