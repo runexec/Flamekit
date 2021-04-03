e37c11c791a7950b090cce90ba74ba12bdaeebb8
Ryan Kelker <1487241+runexec@users.noreply.github.com>
2021-04-03 16:39:50 -0400

    Refactor

--- code/src/extension.ts
+++ code/src/extension.ts
@@ -6,19 +6,19 @@ import * as vscode from 'vscode';
 import * as Injections from './injections'
 
 const AlpineInstance = container.resolve(Injections.Alpine);
-const FragmentInstance = container.resolve(Injections.Fragment);
 const CSSInstance = container.resolve(Injections.CSS);
+const FragmentInstance = container.resolve(Injections.Fragment);
+const PETALInstance = container.resolve(Injections.PETAL);
 const TailwindCSSInstance = container.resolve(Injections.TailwindCSS);
 const TypeScriptInstance = container.resolve(Injections.TypeScript);
-const PETALInstance = container.resolve(Injections.PETAL);
 
 export function activate(context: vscode.ExtensionContext) {
+	AlpineInstance.init({ context: context });
 	CSSInstance.init({ context: context });
 	FragmentInstance.init({ context: context });
-	AlpineInstance.init({ context: context });
+	PETALInstance.init({ context: context });
 	TailwindCSSInstance.init({ context: context });
 	TypeScriptInstance.init({ context: context });
-	PETALInstance.init({ context: context });
 }
 
 // this method is called when your extension is deactivated
