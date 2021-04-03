25f63e2c1a158d72498da134da1ca2eddd1ddf8c
Ryan Kelker <1487241+runexec@users.noreply.github.com>
2021-04-03 04:19:40 -0400

    Add tests

---
+++ code/src/test/suite/fragment/view/fragmentArrayView.test.ts
@@ -0,0 +1,12 @@
+import * as assert from 'assert';
+import * as Constant from '../../../../constant';
+import * as FragmentArrayView from '../../../../fragment/view/fragmentArrayView';
+
+const new_view = (x:string) => new FragmentArrayView.View({ file_name: x }).toString();
+
+describe('Fragment / View / fragmentArrayView.ts ', () => {
+    let test_view = '=f{[abc, 123]}';
+    let test = new_view(test_view);
+    let should_match = "<%= render \"_abc.html\" %>\n<%= render \"_123.html\" %>";
+    it(test_view, () => assert.strictEqual(test, should_match));
+});
