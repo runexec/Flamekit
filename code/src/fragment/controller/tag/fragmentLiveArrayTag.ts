a3d4ee782a5ea9fcb93c722d306fa80f331f2036
Ryan Kelker <1487241+runexec@users.noreply.github.com>
2021-04-07 07:12:45 -0400

    Refactor

--- code/src/fragment/controller/tag/fragmentLiveArrayTag.ts
+++ code/src/fragment/controller/tag/fragmentLiveArrayTag.ts
@@ -1,9 +1,12 @@
 import 'reflect-metadata';
 import {container, singleton} from 'tsyringe';
 
-const Constant : Map<string, any> = container.resolve('ConstantInstance');
+let Constant : Map<string, any>;
 
-export const getTag = (x: string): string => (x.match(Constant.get('FRAGMENT_LIVE_ARRAY_GROUP_REGEX')) || ['',''])[1];
+export const getTag = (x: string): string => {
+    Constant = container.resolve('ConstantInstance');
+    return (x.match(Constant.get('FRAGMENT_LIVE_ARRAY_GROUP_REGEX')) || ['',''])[1];
+}
 
 @singleton()
 export class Injection { getTag: (x:string) => string = getTag }
