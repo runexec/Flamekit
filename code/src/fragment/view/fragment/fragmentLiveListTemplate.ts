6d9a119e7e5a91d6e95b8c4f099a1978cf3e3a76
Ryan Kelker <1487241+runexec@users.noreply.github.com>
2021-04-08 04:09:28 -0400

    Refactor

--- code/src/fragment/view/fragment/fragmentLiveListTemplate.ts
+++ code/src/fragment/view/fragment/fragmentLiveListTemplate.ts
@@ -1,10 +1,20 @@
+import 'reflect-metadata';
+import { singleton, container } from 'tsyringe';
 import * as TemplateClass from './templateClass';
-import * as Store from '../../store/store';
-import * as FragmentFileName from './fileName';
+
+let FileName: { fragmentLiveListFileName: (x: string) => string };
+
+type StorageItem = [tagStart: string, tagEnd: string];
+type StoreCollection = StorageItem[];
+type Storage<T extends StoreCollection> = { push: Function, pop: Function, shift: Function };
+type Store = Storage<StoreCollection> & { FragmentStore: any, StoreType: any };
+
+const Store: Store = container.resolve('fragment.Store');
 
 export class Template extends TemplateClass.Template {
     constructor({ file_name, tag }: { file_name: string, tag: string }) {
         super({ file_name: file_name });
+        FileName = container.resolve('fragment.FileName');
         this.toString = () => {
             const tag_start = '<' + tag + '>',
                 _tag_end = '</' + tag_start.split(' ')[0].split('<')[1],
@@ -14,10 +24,15 @@ export class Template extends TemplateClass.Template {
             return file_name
                 .split(',')
                 .map(x => {
-                    const name = FragmentFileName.fragmentLiveListFileName(x).replace(/\.html/, '');
+                    const name = FileName.fragmentLiveListFileName(x).replace(/\.html/, '');
                     return `${tag_start}<%= live_component ${name} %>${tag_end}`;
                 }).join("\n");
         };
         return this;
     }
+}
+
+@singleton()
+export class Injection {
+    Template = Template
 }
