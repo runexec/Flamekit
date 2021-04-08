3c6c5a867bd5d32c0d20e5b1162eb4ded0b81c40
Ryan Kelker <1487241+runexec@users.noreply.github.com>
2021-04-08 07:23:40 -0400

    Refactor

--- code/src/injection/util/messageInjection.ts
+++ code/src/injection/util/messageInjection.ts
@@ -1,35 +1,25 @@
 import 'reflect-metadata';
-import {singleton, container} from 'tsyringe';
+import { singleton, container } from 'tsyringe';
 import * as Message from '../../util/message';
 
 @singleton()
 class Injection {
-    info : (message:string) => void;
-    debugInfo : (message:string) => void;
-    constructor() {
-        this.info = Message.info;
-        this.debugInfo = Message.debugInfo;
-    }
+    info: (message: string) => void = Message.info;
+    debugInfo: (message: string) => void = Message.debugInfo;
 }
 
 container.register('Util.Message', Injection);
 
 @singleton()
 class InfoInjection {
-    info : (message:string) => void;    
-    constructor() {
-        this.info = Message.info;        
-    }
+    info: (message: string) => void = Message.info;
 }
 
 container.register('Util.Message.info', InfoInjection);
 
 @singleton()
 class DebugInfoInjection {
-    debugInfo : (message:string) => void;    
-    constructor() {
-        this.debugInfo = Message.debugInfo;        
-    }
+    debugInfo: (message: string) => void = Message.debugInfo;
 }
 
 container.register('Util.Message.debugInfo', DebugInfoInjection);
