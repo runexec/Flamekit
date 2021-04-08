b180cb64ab3613acb472c524e181859fd44af439
Ryan Kelker <1487241+runexec@users.noreply.github.com>
2021-04-08 04:17:30 -0400

    Refactor

--- code/src/fragment/view/fragment/fragmentLiveTemplate.ts
+++ code/src/fragment/view/fragment/fragmentLiveTemplate.ts
@@ -1,10 +1,14 @@
+import 'reflect-metadata';
+import { singleton, container } from 'tsyringe';
 import * as TemplateClass from './templateClass';
-import * as FragmentFileName from './fileName';
+
+let FileName: { fragmentLiveFileName: (x: string) => string };
 
 export class Template extends TemplateClass.Template {
     constructor({ file_name }: { file_name: string }) {
+        FileName = container.resolve('fragment.FileName');
         super({ file_name: file_name });
-        this.toString = () => { return `<%= render "${FragmentFileName.fragmentLiveFileName(file_name)}" %>`; }
+        this.toString = () => { return `<%= render "${FileName.fragmentLiveFileName(file_name)}" %>`; }
         return this;
     }
 }
