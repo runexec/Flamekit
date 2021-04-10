b355c2c3700b5990dbe4b277764a9e4677be5cea
Ryan Kelker <1487241+runexec@users.noreply.github.com>
2021-04-10 06:08:52 -0400

    Refactor

--- code/src/injection/utilInjection.ts
+++ code/src/injection/utilInjection.ts
@@ -2,16 +2,17 @@ import 'reflect-metadata';
 import {singleton, container} from 'tsyringe';
 import * as vscode from 'vscode';
 import * as Util from '../util';
+import * as Interface_ from '../interface';
 
 @singleton()
 class Injection extends Object {
     getCallingPath: ({ active_document, fs }: { active_document: vscode.TextDocument; fs?: boolean | undefined; }) => string;
     getActivePath: ({ active_document, fs }: { active_document: vscode.TextDocument; fs?: boolean | undefined; }) => string | undefined;
     getFullActivePath: ({ active_document }: { active_document: vscode.TextDocument; }) => string;
-    getPaths: ({ active_document }: { active_document: vscode.TextDocument; }) => import("/home/on/Projects/vscode-phoenix-webhelpers/runexecFlamekit/code/src/interface").IPaths;
+    getPaths: ({ active_document }: { active_document: vscode.TextDocument; }) => Interface_.IPaths;
     getActiveFileName: ({ active_document, fs }: { active_document: vscode.TextDocument; fs?: boolean | undefined; }) => string | null;
     getDirectory: ({ active_document, fs }: { active_document: vscode.TextDocument; fs?: boolean | undefined; }) => string | null;
-    getWorkingPaths: ({ wsf, active_document }: { wsf: readonly vscode.WorkspaceFolder[]; active_document: vscode.TextDocument; }) => import("/home/on/Projects/vscode-phoenix-webhelpers/runexecFlamekit/code/src/interface").IWorkingPaths;
+    getWorkingPaths: ({ wsf, active_document }: { wsf: readonly vscode.WorkspaceFolder[]; active_document: vscode.TextDocument; }) => Interface_.IWorkingPaths;
     showImporoperFileError: ({ active_document }: { active_document: vscode.TextDocument; }) => void;
     showInvalidPathError: ({ active_document }: { active_document: vscode.TextDocument; }) => void;
     showNoWorkspaceError: () => void;
