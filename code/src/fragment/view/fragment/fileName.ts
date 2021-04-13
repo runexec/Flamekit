b8fae93b58c514ccf71ab0951028032ea7ddb919
Ryan Kelker <1487241+runexec@users.noreply.github.com>
2021-04-13 14:04:53 -0400

    Proper lowercase

--- code/src/fragment/view/fragment/fileName.ts
+++ code/src/fragment/view/fragment/fileName.ts
@@ -14,7 +14,7 @@ export const
         const splitter = /[A-Z][a-z0-9]+/g;
         let component : string = '';
         name.match(splitter)?.forEach((x:string) => {
-            component += x.toLowerCase() + '_';
+            component += x.toLocaleLowerCase() + '_';
         })
         component = component.slice(0,-1);
         return component === '' ? name : component;
